40e604ab931b1008b75d2fd7cd5d37694e310087
David Hawbaker <djhawbaker@gmail.com>
2021-11-09 17:59:18 -0800

    IR Cleaned up main

--- intelligent_robotics/brain_interface/get_data_from_board.py
+++ intelligent_robotics/brain_interface/get_data_from_board.py
@@ -1,31 +1,25 @@
-import argparse
-import time
-import numpy as np
-
-import brainflow
-from brainflow.board_shim import BoardShim, BrainFlowInputParams, LogLevels, BoardIds, BrainFlowError
-from brainflow.data_filter import DataFilter, FilterTypes, AggOperations, WindowFunctions, DetrendOperations
-from brainflow.ml_model import MLModel, BrainFlowMetrics, BrainFlowClassifiers, BrainFlowModelParams
-from brainflow.exit_codes import *
+#!/usr/bin/env python3
+"""
+Class: Intelligent Robotics
+Author: David Hawbaker
+"""
 from BoardInterface import BoardInterface
-
 from EEGModels import EEGNet
 
 
 def main():
 
     board_i = BoardInterface()
-    board_i.start_stream()
 
-    board_i.end_stream()
+    board_i.start_stream()
 
     while True:
         try:
-
-
-
+            data = board_i.get_data()
+            board_i.write_data(data)
 
         except KeyboardInterrupt:
+            board_i.end_stream()
             break
 
 
