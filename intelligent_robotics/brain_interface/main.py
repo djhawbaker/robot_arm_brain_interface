8eb3bfceb396018ae35eb434663372149f4cdacb
David Hawbaker <djhawbaker@gmail.com>
2021-11-23 14:49:31 -0800

    Set to record 2 second intervals

--- intelligent_robotics/brain_interface/main.py
+++ intelligent_robotics/brain_interface/main.py
@@ -17,15 +17,15 @@ def main():
     while True:
         try:
             # EEG data
-            seconds = 10
+            seconds = 2
             BoardShim.log_message(LogLevels.LEVEL_INFO.value, 'start sleeping in the main thread')
             time.sleep(seconds)  # Get data once per second
 
             data = board_i.get_data(samples=400)
             # TODO improve filename. ie read for existing files and increment
             clean_data = board_i.denoise(data, "raw_data_" + str(i))
-            board_i.write_data(data, "raw_data_" + str(i) + ".csv")
-            board_i.write_data(clean_data.to_string, "clean_data_" + str(i) + ".csv")
+            # board_i.write_data(data, "raw_data_" + str(i) + ".csv")
+            board_i.write_data(clean_data.to_string(), "clean_data_" + str(i) + ".csv")
             # board_i.process_input(data)
 
             # Neural Network
