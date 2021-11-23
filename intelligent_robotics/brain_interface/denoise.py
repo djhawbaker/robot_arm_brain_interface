4c9814f9063210bd34c3bda90f3bf91e68e71c36
David Hawbaker <djhawbaker@gmail.com>
2021-11-22 17:41:15 -0800

    denoise file

--- intelligent_robotics/brain_interface/denoise.py
+++ intelligent_robotics/brain_interface/denoise.py
@@ -4,5 +4,10 @@ from BoardInterface import BoardInterface
 
 bi = BoardInterface()
 
+base_file = '~/projects/psu_fall_2021/intelligent_robotics/brain_interface/data/'
+input_file = base_file + 'double_blinks/first_Run/raw_data_1.csv'
+output_file = base_file + 'double_blinks/first_Run/raw_data_1_denoise.csv'
 
-bi.denoise()
+data = bi.read_file(input_file)
+denoised_data = bi.denoise(data, output_file)
+bi.write_data(denoised_data, output_file)
